57a6bce4f830e287ee33c4900f4b70de92aac021
Ryan <ryan.maceachern@gmail.com>
2022-03-02 14:42:38 -0800

    removed placeholder tweet. tweets can now render dynamically. CSS files cleanup.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,33 +7,46 @@ $(document).ready(function() {
 
   // DRIVER CODE
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  }
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
 
-  const createTweetElement = () => {
+  const createTweetElement = (tweet) => {
 
     const tweetElement = `<article>
     <header class="tweet">
       <div class="name">
-        <img src="${tweetData.user.avatars}"/>
-        <strong>"${tweetData.user.name}"</strong>
+        <img src="${tweet.user.avatars}"/>
+        <strong>"${tweet.user.name}"</strong>
       </div>
-      <i class="handle">"${tweetData.user.handle}"</i>
+      <i class="handle">${tweet.user.handle}</i>
     </header>
 
-    <p>"${tweetData.content.text}"</p>
+    <p>"${tweet.content.text}"</p>
 
     <footer>
-      <small>${timeago.format(tweetData.created_at)}</small>
+      <small>${timeago.format(tweet.created_at)}</small>
       <div class="social-icons">
         <figure>
           <i class="fa-solid fa-flag"></i>
@@ -42,15 +55,23 @@ $(document).ready(function() {
         </figure>
       </div>
     </footer>
-  </article>`;
+    </article>`;
 
     return tweetElement;
 
   };
 
+  const renderTweets = (tweets) => {
+    for (const tweet of tweets) {
+      const newTweet = createTweetElement(tweet);
+      $("#tweets-container").append(newTweet);
+    }
 
-  const $tweet = createTweetElement(tweetData);
-  console.log("🚀 ~ file: client.js ~ line 51 ~ $tweet", $tweet);
-  $("#tweets-container").append($tweet);
+  };
+
+  renderTweets(data);
+
+  // const $tweet = createTweetElement(data);
+  // console.log("🚀 ~ file: client.js ~ line 51 ~ $tweet", $tweet);
 
 });
