b31eb8623e4f0bcd6bc1a3eed436543f712282fe
Ryan <ryan.maceachern@gmail.com>
2022-03-01 17:00:38 -0800

    character counter turns red when it reaches 0 and negative numbers

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,9 +6,14 @@ $(document).ready( () => {
   $textArea.on("input", function() {
     const textLength = $(this).val().length;
     const $counter = $(this).siblings().children().last();
-    console.log($counter);
-    textLength <= 140 ? $counter[0].innerHTML = 140 - textLength : $counter[0].innerHTML = -textLength + 140;
-  })
+    if (textLength < 140) {
+      $counter[0].innerHTML = 140 - textLength
+      $($counter).removeClass("red");
+     } else if (textLength >= 140) {
+      $counter[0].innerHTML = -textLength + 140;
+      $($counter).addClass("red");
+     }
+    })
 
   
 })
