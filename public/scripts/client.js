da61c621cef97163d157d9a7da243919bef5e7df
Ryan MacEachern <ryan.maceachern@gmail.com>
2022-03-02 17:41:02 -0800

    new ajax req to load tweets from server on doc ready. basic linting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,39 +10,52 @@ $(document).ready(function() {
     // const userInput = event.target[0].value;
     const serializedForm = $(this).serialize();
     console.log("SerializedForm:", serializedForm);
-    $.post("/tweets/", serializedForm, (response) => {
+    $.post("/tweets", serializedForm, (response) => {
       console.log("serializedForm", serializedForm);
       console.log("Success", response);
       
     })
   });
 
+  const loadTweets = function() {
+    $.get("tweets",  { method: "GET" }, (response) => {
+      console.log("Success!")
+      return renderTweets(response);
+    })
+  }
+  
+  // $.ajax("/tweets", { method: 'GET' })
+  // .then(function (morePostsHtml) {
+  //   console.log('Success: ', morePostsHtml);
+  //   $button.replaceWith(morePostsHtml);
+  loadTweets();
+  
   // DRIVER CODE
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ];
 
   const createTweetElement = (tweet) => {
 
@@ -80,9 +93,5 @@ $(document).ready(function() {
     }
 
   };
-
-  renderTweets(data);
-
-  
 
 });
